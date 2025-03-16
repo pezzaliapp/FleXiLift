ab80994651c3a1d1e0bb51187ed73637760cb960
pezzaliapp <pezzalialessandro@gmail.com>
2025-03-16 15:09:28 +0100

    Update update.js

--- api/update.js
+++ api/update.js
@@ -1,1 +1,11 @@
+fetch("sollevatore.json")
+  .then(response => response.json())
+  .then(data => {
+      document.getElementById("status").innerText = 
+          data.sollevatore ? "🚗 Occupato" : "✅ Libero";
 
+      const newLog = document.createElement("li");
+      newLog.innerText = `Stato: ${data.sollevatore ? "Occupato" : "Libero"} - ${new Date(data.timestamp * 1000).toLocaleString()}`;
+      document.getElementById("log-list").prepend(newLog);
+  })
+  .catch(error => console.error("Errore nel caricamento dati:", error));
